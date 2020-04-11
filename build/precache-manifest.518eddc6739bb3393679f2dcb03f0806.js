75f66af3f847660b1c577aefcfde70d6e95560d5
anvorob <anatolii.vorobiov@gmail.com>
2020-04-11 20:41:57 +1200

    add build

---
+++ build/precache-manifest.518eddc6739bb3393679f2dcb03f0806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73abc78e57e0d9be8a95828559e83dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "004f9daa29e43c9f1722",
+    "url": "/static/css/main.9637328a.chunk.css"
+  },
+  {
+    "revision": "feb3d14453133794b539",
+    "url": "/static/js/2.57819835.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57819835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004f9daa29e43c9f1722",
+    "url": "/static/js/main.b3d04eb1.chunk.js"
+  },
+  {
+    "revision": "fde62f4bf7eaaf967e71",
+    "url": "/static/js/runtime-main.c3bc6126.js"
+  }
+]);
